5149247fef636c10541110cc3597c196a6a674fa
Yi Yang <yiyangmagi@gmail.com>
2020-03-17 23:16:11 -0400

    add newfunction top rate videos and change the layout of index.html

---
+++ top_rate.php
@@ -0,0 +1,18 @@
+
+<?php
+include_once './inc/config.php';
+include_once './inc/db_connect.php';          
+
+
+header("Content-Type: application/json; charset=UTF-8");
+$obj = json_decode($_POST["x"], false);
+
+$conn=connection();
+$sql = "SELECT Vid, Video_name, avg(Rate) as Rate FROM VIDEO JOIN RATING ON Video_id=Vid GROUP BY Vid ORDER BY avg(Rate) DESC LIMIT 5;";
+$result = execute_sql($conn,$sql);
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
+               
+
+?>
