d118abd3eb5546561e060e74a4174f88f18700a7
Yi Yang <yiyangmagi@gmail.com>
2020-03-21 17:18:30 -0400

    Complete tables.php

--- tables.php
+++ tables.php
@@ -14,13 +14,14 @@ $conn = connection();
         table {
             margin: auto;
             border: 3px solid #6aa9ff;
+            
             /* or margin: 0 auto 0 auto */
         }
     </style>
 </head>
 
 <body>
-    <table>
+    <table border="1">
         <tr align="center">
             <td colspan="5">USER Table</td>
         </tr>
@@ -39,6 +40,117 @@ $conn = connection();
         }
         ?>
     </table>
+    </br>
+    <table border="1">
+        <tr align="center">
+            <td colspan="6">VIDEO Table</td>
+        </tr>
+        <tr>
+            <td>Vid</td>
+            <td>Uploader_id</td>
+            <td>Video_name</td>
+            <td>Video_url</td>
+            <td>Upload_date</td>
+            <td>Intro</td>
+        </tr>
+        <?php
+        $sql = " SELECT * FROM VIDEO LIMIT 5;";
+        $result = execute_sql($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row['Vid'] . "</td><td>" . $row['Uploader_id'] . "</td><td>" . $row['Video_name'] . "</td><td>" . $row['Video_url'] . "</td><td>" . $row['Upload_date'] . "</td><td>" . $row['Intro']. "</td></tr>";
+        }
+        ?>
+    </table>
+    </br>
+    <table border="1">
+        <tr align="center">
+            <td colspan="5">COMMENT Table</td>
+        </tr>
+        <tr>
+            <td>Comment_id</td>
+            <td>Video_id/td>
+            <td>User_id</td>
+            <td>Content</td>
+            <td>Release_date</td>
+        </tr>
+        <?php
+        $sql = " SELECT * FROM COMMENT LIMIT 5;";
+        $result = execute_sql($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row['Comment_id'] . "</td><td>" . $row['Video_id'] . "</td><td>" . $row['User_id'] . "</td><td>" . $row['Content'] . "</td><td>" . $row['Release_date'] . "</td></tr>";
+        }
+        ?>
+    </table>
+    </br>
+    <table border="1">
+        <tr align="center">
+            <td colspan="6">VIDEO_TAG Table</td>
+        </tr>
+        <tr>
+            <td>Video_id</td>
+            <td>Tag_id</td>
+        </tr>
+        <?php
+        $sql = " SELECT * FROM VIDEO_TAG LIMIT 5;";
+        $result = execute_sql($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row['Video_id'] . "</td><td>" . $row['Tag_id']. "</td></tr>";
+        }
+        ?>
+    </table>
+    </br>
+    <table border="1">
+        <tr align="center">
+            <td colspan="6">TAG Table</td>
+        </tr>
+        <tr>
+            <td>T_id</td>
+            <td>Tag_name</td>
+        </tr>
+        <?php
+        $sql = " SELECT * FROM TAG LIMIT 5;";
+        $result = execute_sql($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row['T_id'] . "</td><td>" . $row['Tag_name']. "</td></tr>";
+        }
+        ?>
+    </table>
+    </br>
+    <table border="1">
+        <tr align="center">
+            <td colspan="6">RATING Table</td>
+        </tr>
+        <tr>
+            <td>User_id</td>
+            <td>Video_id</td>
+            <td>Rate</td>
+        </tr>
+        <?php
+        $sql = " SELECT * FROM RATING LIMIT 5;";
+        $result = execute_sql($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row['User_id'] . "</td><td>" . $row['Video_id']. "</td><td>" . $row['Rate']. "</td></tr>";
+        }
+        ?>
+    </table>
+    </br>
+    <table border="1">
+        <tr align="center">
+            <td colspan="6">FOLLOWING Table</td>
+        </tr>
+        <tr>
+            <td>User_id</td>
+            <td>Following_id</td>
+        </tr>
+        <?php
+        $sql = " SELECT * FROM FOLLOWING LIMIT 5;";
+        $result = execute_sql($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row['User_id'] . "</td><td>" . $row['Following_id']. "</td></tr>";
+        }
+        ?>
+    </table>
+    </br>
 </body>
 
 </html>
